46e98dd9976ff7c53c267ca83521759b626d2c06
ShuMAhd <shuman@zykaika.ru>
2024-02-23 07:17:03 +0800

    Создан public class MyController

--- src/main/java/com/example/myspringactuatorproject/controller/MyController.java
+++ src/main/java/com/example/myspringactuatorproject/controller/MyController.java
@@ -1,5 +1,13 @@
 package com.example.myspringactuatorproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
 
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello World!";
+  }
 }
