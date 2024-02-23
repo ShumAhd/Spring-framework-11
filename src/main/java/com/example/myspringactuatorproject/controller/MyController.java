1428fa5ca7e08dd29dd5de429e134c8e3dfb3cf6
ShuMAhd <shuman@zykaika.ru>
2024-02-23 09:24:45 +0800

    Добавлен кастомный счётчик вызова /hello

--- src/main/java/com/example/myspringactuatorproject/controller/MyController.java
+++ src/main/java/com/example/myspringactuatorproject/controller/MyController.java
@@ -1,13 +1,23 @@
 package com.example.myspringactuatorproject.controller;
 
+import com.example.myspringactuatorproject.MyMetrics;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+  private final MyMetrics myMetrics;
+
+  @Autowired
+  public MyController(MyMetrics myMetrics) {
+    this.myMetrics = myMetrics;
+  }
+
   @GetMapping("/hello")
   public String hello() {
+    myMetrics.incrementHelloCounter(); // Увеличиваем счётчик
     return "Hello World!";
   }
 }
